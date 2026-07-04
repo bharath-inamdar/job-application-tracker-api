6033c105c6c6f842c8bae85d8dd6339f27a3b2d3
Bharath Inamdar <bharathinamdar@Bharaths-MacBook-Air.local>
2026-07-04 13:36:13 +0530

    Implement in-memory CRUD for job applications

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from app.models import JobApplication
 app = FastAPI()
+applications = []
 
 @app.get("/")
 def home():
@@ -12,3 +14,35 @@ def about():
         "college": "MSRIT",
         "goal": "12-20 LPA"
     }
+@app.get("/dream")
+def dream():
+    return {
+        "goal": "12-20 LPA",
+        "company_type": "Product Based"
+    }
+@app.get("/companies")
+def companies():
+    return [
+        "Google",
+        "Microsoft",
+        "Amazon",
+        "Atlassian",
+        "Adobe"
+    ]
+@app.get("/skills")
+def skills():
+    return {
+  "language": "Python",
+  "framework": "FastAPI",
+  "database": "PostgreSQL"
+}
+@app.post("/applications")
+def create_application(application: JobApplication):
+    applications.append(application.model_dump())
+
+    return {
+        "message": "Application added successfully"
+    }
+@app.get("/applications")
+def get_applications():
+    return applications
---
+++ app/models.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+
+class JobApplication(BaseModel):
+    company: str
+    role: str
+    status: str
