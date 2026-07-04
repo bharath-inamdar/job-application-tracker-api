2f82604085db19a1b8ae1e1437751d3d18746a6a
bharath-inamdar <inamdarbharath567@gmail.com>
2026-07-04 16:16:17 +0530

    Implement complete in-memory CRUD operations

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from app.models import JobApplication
 app = FastAPI()
 applications = []
@@ -46,3 +46,25 @@ def create_application(application: JobApplication):
 @app.get("/applications")
 def get_applications():
     return applications
+@app.delete("/applications/{index}")
+def delete_application(index: int):
+    if index < 0 or index >= len(applications):
+        raise HTTPException(status_code=404, detail="Application not found")
+
+    deleted_application = applications.pop(index)
+
+    return {
+        "message": "Application deleted successfully",
+        "deleted": deleted_application
+    }
+@app.put("/applications/{index}")
+def update_application(index: int, application: JobApplication):
+    if index < 0 or index >= len(applications):
+        raise HTTPException(status_code=404, detail="Application not found")
+
+    applications[index] = application.model_dump()
+
+    return {
+        "message": "Application updated successfully",
+        "updated": applications[index]
+    }
